d8a241472a93c63809b02355a542a481cd20a541
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-11-09 03:01:34 +0000

    bump version
    git-svn-id: https://code.astraw.com/motmot/trunk/wxvideo@232 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup
 
-from motmot_utils import get_svnversion_persistent
-version_str = '0.4.dev%(svnversion)s'
-version = get_svnversion_persistent('wxvideo/version.py',version_str)
-
 setup(name='wxvideo',
       version=version,
       packages = ['wxvideo'],
+      version='0.5.0',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       license='BSD',
