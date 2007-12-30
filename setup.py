a89d4356b3da0c4b712d4de78b54406de9011b1f
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-12-30 18:27:02 +0000

    use setuptools svn versioning
    git-svn-id: https://code.astraw.com/motmot/trunk/wxvideo@382 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='motmot.wxvideo',
       packages = find_packages(),
       namespace_packages = ['motmot'],
-      version='0.5.1',
+      version='0.5.2',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       license='BSD',
