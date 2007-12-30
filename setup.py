980e81614d27740a1debb8ea067c8253d96054d7
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-12-30 15:50:54 +0000

    remove useless line
    git-svn-id: https://code.astraw.com/motmot/trunk/wxvideo@371 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(name='motmot.wxvideo',
       packages = find_packages(),
       namespace_packages = ['motmot'],
-      zip_ok=False,
       version='0.5.1',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
