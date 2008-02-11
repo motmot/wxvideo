18add3fb9320fb24b461f37a5a9dc3a676dfb8bb
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-02-11 05:35:18 +0000

    add long_description attributes to setup.py files
    git-svn-id: https://code.astraw.com/motmot/trunk/wxvideo@477 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name='motmot.wxvideo',
+      description='wx viewer of image sequences',
+      long_description = \
+"""Allows for display and resizing/rotation of images.
+
+This is a subpackage of the motmot family of digital image utilities.
+""",
       packages = find_packages(),
       namespace_packages = ['motmot'],
+      url='http://code.astraw.com/projects/motmot',
       version='0.5.2',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
